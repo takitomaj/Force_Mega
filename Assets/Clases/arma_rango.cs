85f2d28a548e3fcf4a2f73ac296a2d2e86004e5b
takitomaj <takitomaj@gmail.com>
2020-08-20 21:23:40 -0600

    actualisacion diaria
    cambio en personaje y creacion de entidades para spawm de balas

--- Assets/Clases/arma_rango.cs
+++ Assets/Clases/arma_rango.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 
 public class arma_rango : MonoBehaviour
 {
+    public Boolean equipada = true;
     public Rigidbody2D RB_Arma;
     public float jumpPower = 9.25f;
     // Start is called before the first frame update
     void Start()
     {
-        RB_Arma = GetComponent<Rigidbody2D>();
-        var randomInt = UnityEngine.Random.Range(0, 100);
-        int D_I = randomInt;
-        if (D_I>=50) 
+        if (!equipada) 
         { 
-            RB_Arma.AddForce(Vector2.up * jumpPower , ForceMode2D.Impulse);
-            RB_Arma.AddForce(( jumpPower/4) * Vector2.left, ForceMode2D.Impulse);
-        }
-        else
-        {
-            RB_Arma.AddForce(Vector2.up* jumpPower * Vector2.left, ForceMode2D.Impulse);
-            RB_Arma.AddForce((jumpPower / 3) * Vector2.right, ForceMode2D.Impulse);
+            RB_Arma = GetComponent<Rigidbody2D>();
+            var randomInt = UnityEngine.Random.Range(0, 100);
+            int D_I = randomInt;
+            if (D_I >= 50)
+            {
+                RB_Arma.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                RB_Arma.AddForce((jumpPower / 4) * Vector2.left, ForceMode2D.Impulse);
+            }
+            else
+            {
+                RB_Arma.AddForce(Vector2.up * jumpPower * Vector2.left, ForceMode2D.Impulse);
+                RB_Arma.AddForce((jumpPower / 3) * Vector2.right, ForceMode2D.Impulse);
+            }
         }
     }
 
